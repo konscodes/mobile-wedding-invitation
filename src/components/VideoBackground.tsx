1548afaaf4297a47c4e146d0dc229a43d93e7850
Kons M <katashi.rain@gmail.com>
2024-06-29 20:33:39 +0900

    Changed background video params

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,11 +1,10 @@
-// src/components/VideoBackground.tsx
 import React from 'react';
 import './VideoBackground.css';
 
 const VideoBackground: React.FC = () => {
   return (
     <div className="video-background">
-      <video autoPlay loop muted>
+      <video autoPlay loop muted playsInline>
         <source src={`/background.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
