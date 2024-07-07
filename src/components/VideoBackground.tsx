01e9483750043df802a69f79b35fcd3a79c516bd
Kons M <katashi.rain@gmail.com>
2024-07-07 21:24:56 +0900

    Video autoplay fix

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,36 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './VideoBackground.css';
 
 const VideoBackground: React.FC = () => {
-  useEffect(() => {
-    const video = document.querySelector("video");
-
-    const playVideo = () => {
-      if (video) {
-        const playPromise = video.play();
-
-        if (playPromise !== undefined) {
-          playPromise
-            .then(() => {
-              // Autoplay started
-              console.log("Video is playing");
-            })
-            .catch((error) => {
-              // Autoplay was prevented
-              console.error("Autoplay prevented:", error);
-              video.muted = true;
-              video.play().catch((error) => {
-                console.error("Video play failed after setting muted:", error);
-                // No controls will be shown even if it fails to autoplay
-              });
-            });
-        }
-      }
-    };
-
-    playVideo();
-  }, []);
-
   return (
     <div className="video-container">
       <div className="video-background">
